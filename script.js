6e444e06163f7609e5ed45e315651861c5930157
Your Name <you@example.com>
2023-06-22 19:18:31 +0500

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+var userName =prompt('как вас зовут?')
+var age = prompt('Сколько вам лет')
+var pr1 = +prompt('20 - 3 =?')
+var pr2 = +prompt('20 + 3 =?')
+var pr3 = +prompt('20 * 3 =?')
+var pr4 = +prompt('20 / 3 =?')
+var pr5 = +prompt('20 % 3 =?')
+var sb = +prompt('Спасибо')
+console.log(userName);
+console.log(age);    
+console.log('Пример 1: 20 - 3=' + "(ваш ответ " + pr1 +")");           
+console.log('Пример 1: 20 - 3=' + "(ваш ответ " + pr2 +")");           
+console.log('Пример 1: 20 - 3=' + "(ваш ответ " + pr3 +")");           
+console.log('Пример 1: 20 - 3=' + "(ваш ответ " + pr4 +")");           
+console.log('Пример 1: 20 - 3=' + "(ваш ответ " + pr5 +")");    
+console.log("");
+
